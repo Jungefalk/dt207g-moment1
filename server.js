7656d0e52bfc283fef85659a484c2b0ee452d273
Caroline <c.jungefalk@gmail.com>
2025-04-22 20:04:30 +0200

    Lagt till routing för index, form och about

--- server.js
+++ server.js
@@ -3,17 +3,30 @@
  * Caroline Jungefalk Palmgren
  */
 
- //importera paket
- const express = require("express");
- const sqlite3 = require("sqlite3");
- const bodyParser = require("body-parser");
+//importera paket
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const bodyParser = require("body-parser");
 
- //Anslut till databas
- const db = new sqlite3.Database("./db/cv.db");
+//Anslut till databas
+const db = new sqlite3.Database("./db/cv.db");
 
- //Inställningar
- const app = express();
- const port = 3000;
- app.set("view engine", "ejs");         //views
- app.use(express.static("public"));     //public
- app.use(bodyParser.urlencoded({ extended: true }));
+//Inställningar
+const app = express();
+const port = 3000;
+app.set("view engine", "ejs");         //views
+app.use(express.static("public"));     //public
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Routings
+app.get("/", (req, res) => {
+    res.render("index")
+});
+
+app.get("/form", (req, res)=>{
+    res.render("form")
+});
+
+app.get("/about", (req, res)=>{
+    res.render("about")
+});
